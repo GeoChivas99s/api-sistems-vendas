cc19037a758d743dabb073706421a2fcf15dd8b5
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-06-07 00:42:48 +0100

    feat: first steps in Spring Boot

--- src/main/java/org/geoChivas99s/Main.java
+++ src/main/java/org/geoChivas99s/Main.java
@@ -1,29 +1,44 @@
 package org.geoChivas99s;
+
+import org.geoChivas99s.domain.entity.Cliente;
+import org.geoChivas99s.domain.repository.Clients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Main {
-//injeção de dependencias usando o bean
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationName;
-// usando variável global dentro de um arquivo de configurations
-    @Value("${application.name}")
-    private String Teste;
+
+   @Bean
+   public CommandLineRunner init(@Autowired Clients clients){
+       return args -> {
+           Cliente c1 = new Cliente("Lindo Mesmo");
+           Cliente c2 = new Cliente("Marco Pitra");
+
+          clients.salvar(c1);
+          clients.salvar(c2);
 
 
 
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName + Teste ;
-    }
+           System.out.println("--------+----------------");
+
+
+           clients.obterTodos().forEach(e-> System.out.println(e.getNome()));
+
+
+
+       };
+
+   }
     public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
     }
