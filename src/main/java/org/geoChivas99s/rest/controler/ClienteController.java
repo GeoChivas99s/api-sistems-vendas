267fc39e94bcd47f243a9c63cb70be832c090cee
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-06-07 22:00:58 +0100

    feaT: end the client controller

--- src/main/java/org/geoChivas99s/rest/controler/ClienteController.java
+++ src/main/java/org/geoChivas99s/rest/controler/ClienteController.java
@@ -4,14 +4,18 @@ package org.geoChivas99s.rest.controler;
 import org.geoChivas99s.domain.entity.Cliente;
 import org.geoChivas99s.domain.repository.Clients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping(value = "/api/clientes")
 public class ClienteController {
   //  @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET
@@ -26,31 +30,29 @@ public class ClienteController {
     }
 
     @GetMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity getClientById(@PathVariable Integer id){
-        Optional<Cliente> cliente =  clientes.findById(id);
-         if(cliente.isPresent()){
-         return ResponseEntity.ok(cliente.get());
-         }
-            return ResponseEntity.notFound().build();
+    public Cliente getClientById(@PathVariable Integer id){
+     return clientes.findById(id)
+                .orElseThrow(()->
+                 new ResponseStatusException
+                         (HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
 
     }
 
     @PostMapping
-    @ResponseBody
-    public ResponseEntity save(@RequestBody Cliente cliente){
-      Cliente newClient =  clientes.save(cliente);
-      return  ResponseEntity.ok(newClient);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save(@RequestBody Cliente cliente){
+        return  clientes.save(cliente);
+
     }
+    /*
     @GetMapping
     @ResponseBody
     public ResponseEntity getAll(){
         List<Cliente> clientList =  clientes.findAll();
         return  ResponseEntity.ok(clientList);
     }
-
+*/
     @DeleteMapping("/{id}")
-    @ResponseBody
     public ResponseEntity deleteClient(@PathVariable Integer id){
         Optional<Cliente> cliente =  clientes.findById(id);
        if(cliente.isPresent()){
@@ -61,20 +63,27 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
-    @ResponseBody
+
     @PutMapping("/{id}")
-    public  ResponseEntity update(@PathVariable Integer id ,   @RequestBody Cliente cliente){
-        return  clientes.findById(id)
+    public  void update(@PathVariable Integer id ,   @RequestBody Cliente cliente){
+          clientes.findById(id)
                 .map(cl -> {
                     cliente.setId(cl.getId());
                     clientes.save(cliente);
-                    return  ResponseEntity.noContent().build();
-                }).orElseGet( ()-> ResponseEntity.notFound().build() );
+                    return  cl;
+                }).orElseThrow( () -> new ResponseStatusException
+                        (HttpStatus.NOT_FOUND, "Cliente não encontrado!"));
     }
 
 
+    @GetMapping
+    public List<Cliente>  find(Cliente query){
+    ExampleMatcher matcher = ExampleMatcher
+             .matching()
+            .withIgnoreCase()
+            .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+    Example example = Example.of(query, matcher);
+        return clientes.findAll(example);
 
-
-
-
+  }
 }
